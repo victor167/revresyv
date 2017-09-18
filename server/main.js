51510f766d14b848615ccea9af94f12c6abc8a88
Your Name <you@example.com>
2017-09-18 01:10:23 -0500

    ok

---
+++ server/main.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8080,function(){
+	console.log("Servidor corriendo en http://localhost:8080");
+});
+
+//Server Web
+app.use(express.static('public'));
+/*app.get('/',function(req,res){
+	res.status(200).send("HOLA");
+});
+*/
+
+//Server Web Socket
+io.on('connection',function(socket){
+	console.log('Alguien se ha conectado con Socket');
+
+	socket.on('set-service',function(data){
+		console.log("service",data);
+	});
+
+	/*socket.emit('messages',{
+		'name': 'Victor Recines'
+	});*/
+});
